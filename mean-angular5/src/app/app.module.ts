c576d72a323796eb50b5b7a6b81d46dc626e5918
Naveen Kumar <metanaveen@outlook.com>
2018-02-23 18:15:43 +0530

    Angular 5 pages for CRUD operations. Course Completed :)

--- mean-angular5/src/app/app.module.ts
+++ mean-angular5/src/app/app.module.ts
@@ -8,12 +8,30 @@ import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
 
 const appRoutes: Routes = [
   {
     path: 'books',
     component: BookComponent,
     data: { title: 'Book List' }
+  },
+  {
+    path: 'book-details/:id',
+    component: BookDetailComponent,
+    data: { title: 'Book Details' }
+  },
+  {
+    path: 'book-create',
+    component: BookCreateComponent,
+    data: { title: 'Create Book' }
+  },
+  {
+    path: 'book-edit/:id',
+    component: BookEditComponent,
+    data: { title: 'Edit Book' }
   },
   { path: '',
     redirectTo: '/books',
@@ -24,7 +42,10 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
